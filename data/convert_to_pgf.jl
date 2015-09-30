487419d2e4aa50749f1a0dc60fa0b190a68aaa37
Sebastian Mai <bastinat0r@bastinat0r.de>
2015-09-30 14:17:03 +0200

    actually convert csv to pgf with julia script

--- data/convert_to_pgf.jl
+++ data/convert_to_pgf.jl
@@ -1,8 +1,33 @@
+#!/usr/bin/julia
 using Gadfly
 using DataFrames
+using ArgParse
 
-print ARGS
-df = readtable(ARGS[0]+".csv")
-df = df[ df[:dqf] .> 0, :]
-p = plot(df, x=["dqf"], y=["range"])
-draw(PGF(ARGS[0]+".pgf", 10cm 10cm, true),p)
+
+function parseComandline()
+	args_settings = ArgParseSettings()
+	@add_arg_table args_settings begin
+		"--csv", "-c"
+			help="csv file to be parsed"
+			arg_type = String
+			required = true
+		"--outfile", "-o"
+			help="output filename"
+			arg_type = String	
+	end
+
+	return parse_args(args_settings)
+end
+
+function main()
+	parsed_args = parseComandline()
+	println(parsed_args)
+	s = parsed_args["csv"]
+	df = readtable(s)
+	df = df[ df[:dqf] .> 0, :]
+	p = plot(df, x=["dqf"], y=["range"])
+	outfile=parsed_args["outfile"]
+	draw(PGF(outfile, 10cm, 10cm, true),p)
+end
+
+main()
