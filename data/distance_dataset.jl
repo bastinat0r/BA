a4020638e70e036413041e6b7a4f4d16c0273d34
Sebastian Mai <bastinat0r@bastinat0r.de>
2015-10-29 16:51:02 +0100

    add datasets
    - y*: 200 values each at given distance with new freq settings
    - 1m_new_freq, many values for 1m with new freq settings

--- data/distance_dataset.jl
+++ data/distance_dataset.jl
@@ -1,10 +1,8 @@
 #!/usr/bin/julia -i
 using Gadfly, DataFrames
 filelist = readdir()
-xcsv = filter(x -> ismatch(r"^x.*\.csv", x), filelist)
+xcsv = filter(x -> ismatch(r"^y.*\.csv", x), filelist)
 dfs = [readtable(x) for x in xcsv]
 df = reduce(vcat, dfs)
-df = df[ df[:dqf] .> 0, :]
-df = df[ df[:range] .< 10000, :]
 set_default_plot_format(:svg)
 set_default_plot_size(32cm, 20cm)
