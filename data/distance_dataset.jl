2044563d71b2fe4afbe3dd950c228c94a1896917
Sebastian Mai <bastinat0r@bastinat0r.de>
2015-10-30 12:00:59 +0100

    adding new data for new freq

--- data/distance_dataset.jl
+++ data/distance_dataset.jl
@@ -1,8 +1,11 @@
 #!/usr/bin/julia -i
 using Gadfly, DataFrames
-filelist = readdir()
-xcsv = filter(x -> ismatch(r"^y.*\.csv", x), filelist)
-dfs = [readtable(x) for x in xcsv]
-df = reduce(vcat, dfs)
 set_default_plot_format(:svg)
 set_default_plot_size(32cm, 20cm)
+filelist = readdir()
+xcsv = filter(x -> ismatch(r"^x.*\.csv", x), filelist)
+dfs = [readtable(x) for x in xcsv]
+xdf = reduce(vcat, dfs)
+ycsv = filter(x -> ismatch(r"^y.*\.csv", x), filelist)
+dfs = [readtable(x) for x in ycsv]
+ydf = reduce(vcat, dfs)
